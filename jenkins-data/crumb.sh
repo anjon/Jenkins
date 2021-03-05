bc43a5711c3b39a087df41264e30e45826c7738d
Anjon Sarker <anjon@example.com>
2021-03-05 20:47:03 +0000

    Deleted temporary file

--- jenkins-data/crumb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-crumb=$(curl -u "jenkins:passw0rd" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-#curl -u "jenkins:passw0rd" -H "$crumb" -X POST http://jenkins.local:8080/job/ENV/build?delay=0sec
-curl -u "jenkins:passw0rd" -H "$crumb" -X POST  http://jenkins.local:8080/job/Ansible-Users-DB/buildWithParameters?AGE=35
